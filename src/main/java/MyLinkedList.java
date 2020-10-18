f937eb444df1a58dd978464bf7b9f72732fac33e
PrajwalRao29 <raoprajrao@gmail.com>
2020-10-18 13:32:23 +0530

    Added functionality to find frequency of occurence in string

---
+++ src/main/java/MyLinkedList.java
@@ -0,0 +1,37 @@
+public class MyLinkedList {
+    public MyNode head;
+
+    public void add(String key,int value) {
+        MyNode node=new MyNode(key,value);
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            MyNode temp=this.head;
+            while(temp.next!=null)
+            {
+                temp=temp.next;
+            }
+            temp.next=node;
+        }
+    }
+
+    public void print() {
+        MyNode n = this.head;
+        while (n != null) {
+            System.out.print("WORD = "+ n.key +" NO. OF TIMES OCCURED = "+n.value);
+            System.out.println();
+            n = n.next;
+        }
+    }
+
+    public boolean search(String key) {
+        MyNode temp = this.head;
+        while (temp != null) {
+            if (temp.key.equalsIgnoreCase(key))
+                return true;
+            temp = temp.next;
+        }
+        return false;
+    }
+}
+
---
+++ src/main/java/MyNode.java
@@ -0,0 +1,10 @@
+public class MyNode{
+    String key;
+    int value;
+    MyNode next;
+
+    public MyNode(String key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
